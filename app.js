2513b30875c5be98c8270b2d9e6cec3c366fe9ca
masataka <hanayoriinko@gmail.com>
2020-05-06 14:12:27 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+var orage = 100;
+var apple = 120;
+
+if (orange < apple){
+	alert('みかんをの値段りんごより安い');
+}
+else if (orange == apple){
+	alert('みかんとりんごが同じ値段');
+}
+else{
+	alert('みかんの値段がりんごより高い');
+}
